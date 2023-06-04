96d9640e8bc1b165f79a15968dace1b2f42e93ac
Ryan <ryan.brower28@gmail.com>
2023-06-03 23:53:14 -0600

    more resume

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Nav from "./pages/Nav";
-import Home from "./pages/Home";
+// import Nav from "./pages/Nav";
+// import Home from "./pages/Home";
 import Resume from "./pages/Resume";
 import Game from "./pages/Game";
 import NoPage from "./pages/NoPage";
@@ -10,9 +10,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Nav />}>
-          <Route index element={<Home />} />
-          <Route path="resume" element={<Resume />} />
+        <Route path="/">
+          <Route index element={<Resume />} />
           <Route path="game" element={<Game />} />
           <Route path="*" element={<NoPage />} />
         </Route>
